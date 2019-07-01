abd89f036b4bc1308ac9a0b7b3a72b66fe9321f6
Shahab <tausiq19@gmail.com>
2019-07-01 15:19:14 -0400

    adds timer ui

---
+++ lib/timer/timer_actions.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_plate/timer/bloc/bloc.dart';
+
+import 'bloc/timer_bloc.dart';
+
+class Actions extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: _mapStateToActionButtons(
+        timerBloc: BlocProvider.of<TimerBloc>(context),
+      ),
+    );
+  }
+
+  List<Widget> _mapStateToActionButtons({
+    TimerBloc timerBloc,
+  }) {
+    final TimerState state = timerBloc.currentState;
+    if (state is Ready) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.dispatch(Start(duration: state.duration)),
+        ),
+      ];
+    }
+    if (state is Running) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.pause),
+          onPressed: () => timerBloc.dispatch(Pause()),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.dispatch(Reset()),
+        ),
+      ];
+    }
+    if (state is Paused) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.play_arrow),
+          onPressed: () => timerBloc.dispatch(Resume()),
+        ),
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.dispatch(Reset()),
+        ),
+      ];
+    }
+    if (state is Finished) {
+      return [
+        FloatingActionButton(
+          child: Icon(Icons.replay),
+          onPressed: () => timerBloc.dispatch(Reset()),
+        ),
+      ];
+    }
+    return [];
+  }
+}
---
+++ lib/timer/timer_page.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_plate/timer/timer_actions.dart';
+
+import 'bloc/timer_bloc.dart';
+
+class TimerPage extends StatelessWidget {
+  static const TextStyle timerTextStyle = TextStyle(
+    fontSize: 60,
+    fontWeight: FontWeight.bold,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final TimerBloc _timerBloc = BlocProvider.of<TimerBloc>(context);
+    return Scaffold(
+      appBar: AppBar(title: Text('Flutter Timer')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.symmetric(vertical: 100.0),
+            child: Center(
+              child: BlocBuilder(
+                bloc: _timerBloc,
+                builder: (context, state) {
+                  final String minutesStr = ((state.duration / 60) % 60)
+                      .floor()
+                      .toString()
+                      .padLeft(2, '0');
+                  final String secondsStr =
+                      (state.duration % 60).floor().toString().padLeft(2, '0');
+                  return Text(
+                    '$minutesStr:$secondsStr',
+                    style: TimerPage.timerTextStyle,
+                  );
+                },
+              ),
+            ),
+          ),
+          BlocBuilder(
+            condition: (previousState, currentState) =>
+                currentState.runtimeType != previousState.runtimeType,
+            bloc: _timerBloc,
+            builder: (context, state) => Actions(),
+          ),
+        ],
+      ),
+    );
+  }
+}
