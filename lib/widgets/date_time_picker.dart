d025175c382186f8a00bcb3247c14d3218ec7f36
Shahab <tausiq19@gmail.com>
2019-09-24 15:20:16 -0400

    adds time picker

--- lib/widgets/date_time_picker.dart
+++ lib/widgets/date_time_picker.dart
@@ -107,3 +107,40 @@ class DatePicker extends StatelessWidget {
     );
   }
 }
+
+class TimePicker extends StatelessWidget {
+  const TimePicker(
+      {Key key, this.labelText, this.selectedTime, this.selectTime})
+      : super(key: key);
+
+  final String labelText;
+  final TimeOfDay selectedTime;
+  final ValueChanged<TimeOfDay> selectTime;
+
+  Future<Null> _selectTime(BuildContext context) async {
+    final TimeOfDay picked =
+    await showTimePicker(context: context, initialTime: selectedTime);
+    if (picked != null && picked != selectedTime) selectTime(picked);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextStyle valueStyle = Theme.of(context).textTheme.title;
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: <Widget>[
+        Expanded(
+          flex: 4,
+          child: InputDropdown(
+            labelText: labelText,
+            valueText: selectedTime.format(context),
+            valueStyle: valueStyle,
+            onPressed: () {
+              _selectTime(context);
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
