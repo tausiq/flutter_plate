172ac748033e49d0d461df5b2ee027a43f7e7c22
Shahab <tausiq19@gmail.com>
2019-12-24 01:05:08 -0500

    adds test cases for workout bloc

---
+++ test/workout/workout_bloc_test.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_plate/workout/bloc/bloc.dart';
+import 'package:flutter_plate/workout/workout_repository.dart';
+import 'package:mockito/mockito.dart';
+import 'package:test/test.dart';
+
+class MockWorkoutRepository extends Mock implements WorkoutRepository {}
+
+void main() {
+  WorkoutBloc workoutsBloc;
+  MockWorkoutRepository workoutRepository;
+
+  setUp(() {
+    workoutRepository = MockWorkoutRepository();
+    workoutsBloc = WorkoutBloc(workoutsRepository: workoutRepository);
+  });
+
+  test('initial state is correct', () {
+    expect(workoutsBloc.initialState, WorkoutsLoading());
+  });
+
+  test('dispose does not emit new states', () {
+    expectLater(
+      workoutsBloc.state,
+      emitsInOrder([WorkoutsLoading(), emitsDone]),
+    );
+    workoutsBloc.close();
+  });
+}
